0f08fc508a925284bf4e794f6a5dada5856e92a6
doofin <8177dph@gmail.com>
2025-02-03 22:22:51 +0100

    feat: Add fileIO.createFile() function for creating a test file

--- src/main/scala/vscextension/io/fileIO.scala
+++ src/main/scala/vscextension/io/fileIO.scala
@@ -1,7 +1,13 @@
 package vscextension.io
+import typings.node.fsMod
 
 /** IO utilities for input/output operations, like creating files/folders
   *
   * This object contains utilities for IO operations.
   */
-object fileIO {}
+object fileIO {
+  def createFile() = {
+    fsMod.mkdirSync("test")
+    fsMod.writeFileSync("test/test.txt", "Hello, World!")
+  }
+}
